604044ca8177cc9dd88454c0318dac5f0e1118a3
Maxim Gribov (mcsim4s) <mcsim1993@gmail.com>
2023-06-25 12:16:59 +0300

    better for linux

--- setup.sh
+++ setup.sh
@@ -1,23 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
 set_up_mac() {
 	echo "Installing brew..."
 	which -s brew
 	[[ $? != 0 ]] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-	echo "Intalling oh my zsh..."
-	[ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
-	
-	echo "Linking .dotfiles..."
-	[ -f "$HOME/.zshrc" ] && rm ~/.zshrc
+	install_oh_my_zsh
+}
+
+set_up_linux() {
+	echo "Installing zsh..."
+	which zsh
+	[[ $? != 0 ]] && sudo apt install zsh -y
+
+	install_oh_my_zsh
+}
+
+install_oh_my_zsh() {
+	if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+		echo "Installing ohmyzsh..."
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+		echo "exec zsh" >> ~/.bashrc
+	else
+		echo "Zsh is already installed. Moving on"
+	fi
+
+	echo "Linking zsh .dotfiles..."
+	[[ -f "$HOME/.zshrc" ]] && rm ~/.zshrc
 	ln zsh/.zshrc ~/.zshrc
-	
 }
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        echo "settin up linux env"
+	echo "Detected os is linux. Setting up linux environment..."
+    set_up_linux
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-		set_up_mac
+	echo "Detected os is macOS. Setting up mac environment..."
+	set_up_mac
 fi
 
 echo "Configuring git"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -8,16 +8,20 @@ plugins=(git)
 source $ZSH/oh-my-zsh.sh
 
 export GOPATH="${HOME}/.go"
-export PATH=$PATH:$(go env GOPATH)/bin
+which go
+[[ $? == 0 ]] && export PATH=$PATH:$(go env GOPATH)/bin
 
 export PATH="$HOME/.jenv/bin:$PATH"
-eval "$(jenv init -)"
+which jenv
+[[ $? == 0 ]] && eval "$(jenv init -)"
 
-export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+which rustc
+[[ $? == 0 ]] && export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+
 [ -f "$HOME/.ghcup/env" ] && source "$HOME/.ghcup/env" # ghcup-env
 
 export PATH=$PATH:$HOME/.local/bin
-ssh-add -K ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
 
 [ -f "$HOME/.zsh_local" ] && source "$HOME/.zsh_local"
 [ -f "aliases" ] && source "aliases"
